60a793e0eef5b360f9edf0dd1bccef3594ba1438
thewich25 <casadeporteonline@gmail.com>
2026-01-07 09:13:27 -0400

    actualizando bakc

--- config/db.js
+++ config/db.js
@@ -7,7 +7,7 @@ dotenv.config();
 const dbConfig = {
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
-  password: process.env.DB_PASSWORD || '',
+  password: process.env.DB_PASSWORD || 'root',
   database: process.env.DB_NAME || 'entradas_db',
   waitForConnections: true,
   connectionLimit: 10,
